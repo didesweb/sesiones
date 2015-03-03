8a3f5d398d341e716f1bc49de3d95b2bf819bc41
Roque Arnas <roque.arnas@didesweb.com>
2015-03-04 00:45:50 +0100

    Create exit.php

---
+++ exit.php
@@ -0,0 +1,24 @@
+<?php 
+session_start();
+session_destroy();
+?>
+<!DOCTYPE HTML>
+	<!-- Didesweb -->  
+	<!-- Diseño y desarrollo web -->  
+	<!-- http://didesweb.com/-->  
+	<!-- Este obra está protegida bajo licencia Creative Commons Attribution --> 
+<html lang="es">
+<head>
+<meta charset="UTF-8">
+<title>Didesweb, control de sesion de usuarios en PHP</title>
+</head>
+<body>
+<h1>&copy; Didesweb, control de sesion de usuarios en PHP</h1>
+<p>
+<a href="index.php">Inicio</a>
+<a href="sesion.php">Sesion</a>
+<a href="exit.php">Salir</a>
+</p>
+<p>Has cerrado la sesión</p>
+</body>
+</html>
